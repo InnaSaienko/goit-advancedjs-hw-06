55ddaf3111c3d8eecf3c54f9d02d3b54ef5be2af
Inna Saienko <inna.saienko.nl@gmail.com>
2026-06-22 10:20:12 +0200

    feat: Pick specific types from defined AllType

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -5,19 +5,23 @@
 */
 
 type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number
+    name: string;
+    position: number;
+    color: string;
+    weight: number
 }
 
-function compare (top, bottom): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  }
+function compare<
+    Top extends Pick<AllType, "name" | "color">,
+    Bottom extends Pick<AllType, "position" | "weight">
+>
+(top: Top, bottom: Bottom): AllType {
+    return {
+        name: top.name,
+        color: top.color,
+        position: bottom.position,
+        weight: bottom.weight,
+    }
 }
 
 export {};
